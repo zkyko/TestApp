be8a85b7bab5c6305786544211295202ba3207c4
Nischal Bhandari <evl19@txstate.edu>
2025-09-20 17:01:57 -0500

    Restore source code and set up CI/CD with API key from secrets

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TravelPlan/',
+  base: '/TravelApp/',
   server: {
     port: 3000,
     open: true
